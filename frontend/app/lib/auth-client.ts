d3c07d55d57bfd6097dd83ab2bdacfb5ea4634b1
saksham singh <sa9336160@gmail.com>
2026-06-28 01:50:55 +0530

    fix: auth-client comma fix

--- frontend/app/lib/auth-client.ts
+++ frontend/app/lib/auth-client.ts
@@ -3,9 +3,6 @@ import { adminClient } from "better-auth/client/plugins";
 import { polarClient } from "@polar-sh/better-auth/client";
 
 export const authClient = createAuthClient({
-  //baseURL: "http://localhost:5000",
-
-  baseURL: import.meta.env.VITE_API_URL
-
+  baseURL: import.meta.env.VITE_API_URL,
   plugins: [adminClient(), polarClient()],
 });
