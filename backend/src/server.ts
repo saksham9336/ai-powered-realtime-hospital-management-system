fc5e892b6b430df6ece04f43baa5bae98eb3684d
saksham singh <sa9336160@gmail.com>
2026-06-28 02:08:51 +0530

    fix: add all vercel urls to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -49,9 +49,10 @@ app.use(
    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
     //credentials: true,
  // }),
- cors({
+cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://ai-powered-realtime-hospital-manage.vercel.app",
       "https://ai-powered-realtime-hospital-management-system-76xywr418.vercel.app",
       "http://localhost:5173",
     ],
